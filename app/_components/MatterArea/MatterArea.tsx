a8815d24313bcbf400374576675bcd73eb634317
SatooRu65536 <ryuto4619@gmail.com>
2023-12-29 01:08:39 +0900

    add: blog.satooru.me body

--- app/_components/MatterArea/MatterArea.tsx
+++ app/_components/MatterArea/MatterArea.tsx
@@ -14,6 +14,7 @@ import {
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import satooruDev from "./satooru-dev.svg";
 import satooruMe from "./satooru-me.svg";
+import blogSatooruMe from "./blog-satooru-me.svg";
 
 type WindowSize = {
   width: number;
@@ -26,6 +27,10 @@ export default function Main() {
     width: 0,
     height: 0,
   });
+
+  function openLink(url: string) {
+    window.open(url, "_blank");
+  }
 
   function handleResize() {
     setWindowSize({
@@ -106,7 +111,7 @@ export default function Main() {
       }
     );
     const satooruMeRect = Bodies.rectangle(
-      clientWidth * 0.5,
+      clientWidth * 0.2,
       clientHeight * 0.1,
       180,
       22,
@@ -122,14 +127,32 @@ export default function Main() {
         },
       }
     );
+    const blogSatooruMeRect = Bodies.rectangle(
+      clientWidth * 0.8,
+      clientHeight * 0.1,
+      180,
+      22,
+      {
+        friction: 0.0001,
+        restitution: 0.01,
+        render: {
+          sprite: {
+            texture: blogSatooruMe.src,
+            xScale: 0.6,
+            yScale: 0.6,
+          },
+        },
+      }
+    );
     Composite.add(world, [
-      Bodies.rectangle(clientWidth * 0.3, clientHeight * 0.6, 60, 60, {
+      sensor,
+      Bodies.rectangle(clientWidth * 0.2, clientHeight * 0.6, 60, 60, {
         frictionAir: 0.001,
       }),
       Bodies.rectangle(clientWidth * 0.5, clientHeight * 0.6, 60, 60, {
         frictionAir: 0.05,
       }),
-      Bodies.rectangle(clientWidth * 0.7, clientHeight * 0.6, 60, 60, {
+      Bodies.rectangle(clientWidth * 0.8, clientHeight * 0.6, 60, 60, {
         frictionAir: 0.01,
       }),
       Bodies.rectangle(
@@ -141,9 +164,9 @@ export default function Main() {
           isStatic: true,
         }
       ),
-      sensor,
       satooruDevRect,
       satooruMeRect,
+      blogSatooruMeRect,
       mouseConstraint,
     ]);
 
@@ -152,8 +175,18 @@ export default function Main() {
       max: { x: clientWidth, y: clientHeight },
     });
 
+    let mouseDownTime = 0;
+    let lastClickBody: Body | null = null;
+
     Events.on(mouseConstraint, "mousedown", (e) => {
-      if (mouseConstraint.body !== sensor) return;
+      mouseDownTime = Date.now();
+      lastClickBody = mouseConstraint.body;
+
+      if (mouseConstraint.body !== sensor) {
+        console.log("sensor");
+        return;
+      }
+      console.log("other");
 
       const composite = Composite.create();
       Composite.add(world, composite);
@@ -197,16 +230,33 @@ export default function Main() {
       }
     });
 
+    Events.on(mouseConstraint, "mouseup", () => {
+      if (Date.now() - mouseDownTime > 100) return;
+
+      if (lastClickBody === satooruMeRect) {
+        openLink("https://satooru.me");
+      } else if (lastClickBody === blogSatooruMeRect) {
+        openLink("https://blog.satooru.me");
+      }
+    });
+
     Events.on(engine, "collisionEnd", (e) => {
       e.pairs.forEach((pair) => {
         const { bodyA, bodyB } = pair;
+        const notRemoveBodies = [
+          satooruDevRect,
+          satooruMeRect,
+          blogSatooruMeRect,
+        ];
+
         if (bodyA !== sensor && bodyB !== sensor) return;
-        if (bodyA === satooruDevRect || bodyB === satooruDevRect) {
-          Body.setPosition(satooruDevRect, { x: clientWidth * 0.5, y: 0 });
-          Body.setSpeed(satooruDevRect, 0);
-        } else {
-          const removeBody = bodyA !== sensor ? bodyB : bodyA;
-          Composite.remove(world, removeBody);
+        if (
+          notRemoveBodies.includes(bodyA) ||
+          notRemoveBodies.includes(bodyB)
+        ) {
+          const body = bodyA !== sensor ? bodyA : bodyB;
+          Body.setPosition(body, { x: clientWidth * 0.5, y: 0 });
+          Body.setSpeed(body, 0);
         }
       });
     });
